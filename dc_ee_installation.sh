d0c8a0275baea00f98b6fcc1c475c0c490d0e8e2
roshankadasani <roshan_123@csu.fullerton.edu>
2017-09-20 17:05:07 -0400

    docker ee installation

--- dc_ee_installation.sh
+++ dc_ee_installation.sh
@@ -2,4 +2,20 @@
 
 #Script to install docker EE
 
+echo 'Running Script...'
+
+echo 'Downloading Docker EE 17.02...'
+
 wget "https://storebits.docker.com/ee/trial/sub-3fceb45d-fdd5-481a-b04f-ca628652997b/ubuntu/dists/xenial/pool/stable-17.06/amd64/docker-ee_17.06.1~ee~2-0~ubuntu_amd64.deb"
+
+echo 'Installing libltdl7...'
+
+sudo apt-get install libltdl7
+
+echo 'Installing Docker EE'
+
+sudo dpkg -i docker-ee_17.06.1~ee~2-0~ubuntu_amd64.deb
+
+docker info
+
+echo 'Exiting Script'

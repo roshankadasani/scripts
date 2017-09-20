8fff94e1ccf7122b5add762666738fd2b402956d
roshankadasani <roshan_123@csu.fullerton.edu>
2017-09-20 17:02:29 -0400

    docker ee installation - basic

---
+++ dc_ee_installation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Script to install docker EE
+
+wget "https://storebits.docker.com/ee/trial/sub-3fceb45d-fdd5-481a-b04f-ca628652997b/ubuntu/dists/xenial/pool/stable-17.06/amd64/docker-ee_17.06.1~ee~2-0~ubuntu_amd64.deb"
